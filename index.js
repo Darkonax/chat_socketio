e67147169c7ef8cf9e61f53b91a22d12f6fabd4e
Darkonax <42380766+Darkonax@users.noreply.github.com>
2023-12-29 11:10:49 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,141 +1,163 @@
 
-var username = ""
-var userss = []
-var socket
 
-$(document).ready( function(){
-    verificaruseronline()
-})
+let username = "";
+let users = [];
+let socket;
 
+$(document).ready(() => {
+
+});
+
+// Use arrow function for click event
 $("#btnenviarid").on('click', () => {
-    
-    if(socket.connected){
-        const text = $("#textinputid").val().trim()
+    if (typeof socket?.connected !== "undefined") {
+        const text = $("#textinputid").val().trim();
         const msg = {
             msg: text,
-            name:username,
-            tipe: "1"
+            name: username,
+            tipe: "1",
+        };
+        socket.emit('message', msg);
+        $("#textinputid").val("");
+    } else {
+        const msg = {
+            name: "Try to 'Conectar' button first. Server is disconnected",
+            msg: "",
+            tipe: "3"
         }
-        socket.emit('message', msg)
-        $("#textinputid").val("")
-    }else{
-        console.log('Dont have a Connection')
+        sendMessageToTextarea(msg)
+        $("#btnconnectid")
+        .addClass("border border-danger")
+        console.log('Don\'t have a connection');
     }
+});
 
-})
-
-$("#btnconnectid").on('click', function(){
-    
-    const nick = $("#nicknameid").val()
-    if(nick.length < 3){
-        $("#alertid").html("Coloque um Nome antes de proceguir, minimo 3 charactere")
-        $("#nicknameid").removeClass("border border-dark")
-        $("#nicknameid").addClass("border border-danger")
-    }else{
-        username = $('#nicknameid').val().trim()
-        $("#nicknameid").removeClass("border border-danger border-dark")
-        $("#nicknameid").addClass("border border-primary")
-        $("#nicknameid").attr("disabled", "disabled")
-        $("#alertid").html("trying to Connect ...")
-        $("#alertid").removeClass("alert-danger")
-        $("#alertid").addClass("alert-warning")
-        initconection()
+// Use arrow function for click event
+$("#btnconnectid").on('click', () => {
+    const nick = $("#nicknameid").val();
+    if (nick.length < 3) {
+        $("#alertid")
+            .html("Coloque um Nome antes de prosseguir, mínimo 3 caracteres")
+            .removeClass("border border-dark")
+            .addClass("border border-danger");
+    } else {
+        username = $('#nicknameid').val().trim();
+        $("#nicknameid")
+            .removeClass("border border-danger border-dark")
+            .addClass("border border-primary")
+            .attr("disabled", "disabled");
+        $("#alertid")
+            .html("Trying to Connect to the Server ...")
+            .removeClass("alert-danger")
+            .addClass("alert-warning");
+        initConnection();
     }
-})
+});
 
-function initconection(){
+// Use function expression instead of function declaration
+const initConnection = () => {
 
-    socket = io('http://localhost:3001')
+    socket = io('localhost:3001');
 
-    socket.on('connect', () => {
-
-        $("#alertid").removeClass("alert-danger")
-        $("#alertid").removeClass("alert-warning")
-        $("#statusserverid").removeClass("text-danger")
-        $("#statusserverid").addClass("text-success")
-        $("#alertid").addClass("alert-success")
-        $("#alertid").html("Connection Establishment")
-        
-        $("#nicknameid").attr("disabled", "disabled")
-        $("#btnconnectid").attr("disabled", "disabled")
-
-        socket.emit('nickname', username)
-
-        socket.on('message', (message) => {
-
-            enviarparatextarea(message)
-        })
-
-        socket.on('updateusers', (users) => {
-            userss = users
-            $("#userid").empty()
-            userlogados()
-        } )
-
-        socket.on('discon', (user) => {
-            const olduser = {
-                name: user,
-                tipe: "3"
-            }
-            enviarparatextarea(olduser)
-
-        })
-
-
+    socket.io.on('error', (error) => {
+        const message = {
+            msg: "",
+            tipe: "3",
+            name: "Reconnection attempt ..." 
+        }
+        sendMessageToTextarea(message);
     })
 
-}
+    socket.on('connect', () => {
+        $("#alertid")
+            .removeClass("alert-danger alert-warning")
+            .addClass("alert-success")
+            .html("Connection Establishment");
 
+        $("#nicknameid, #btnconnectid").attr("disabled", "disabled");
 
-function verificaronlines(){
-    socket.emit('updateuser')
-    scrollelement()
-}
+        socket.emit('nickname', username);
 
-function userlogados() {
+        socket.on('message', (message) => {
+            sendMessageToTextarea(message);
+        });
 
-    var htmls = ``
-    userss.forEach(user => {
-        htmls = htmls + `<div>${user.name} <i class="bi bi-circle-fill text-success"></i></div>`
+        socket.on('updateusers', (onlineUsers) => {
+            users = onlineUsers;
+            $("#userid").empty();
+            displayOnlineUsers();
+        });
+
+        socket.on('discon', (disconnectedUser) => {
+            const oldUser = {
+                name: `${disconnectedUser} is disconnected`,
+                tipe: "3",
+            };
+            sendMessageToTextarea(oldUser);
+        });
+
+        socket.on('connect_error', function(err) {
+            console.log("ERROR")
+        });
+
     });
 
-    $("#userid").html($("#userid").html() + htmls)
-}
+};
 
-function enviarparatextarea(msg) {
-    const message = msg
-    if(message.tipe == "1"){
-        $("#Chatareaid").html($("#Chatareaid").html() +
-        `
+// Use camelCase for function names
+const verifyOnlineUsers = () => {
+    socket.emit('updateuser');
+    scrollElement();
+};
+
+// Use camelCase for function names
+const displayOnlineUsers = () => {
+    let html = "";
+    users.forEach((user) => {
+        html += `<div>${user.name} <i class="bi bi-circle-fill text-success"></i></div>`;
+    });
+
+    $("#userid").html($("#userid").html() + html);
+};
+
+// Use camelCase for function names and switch statement for message type
+const sendMessageToTextarea = (msg) => {
+    const message = msg;
+    switch (message.tipe) {
+        case "1":
+            $("#Chatareaid").html($("#Chatareaid").html() +
+                `
         <div class="mb-2 p-2 bg-dark rounded">
             <div>
                 <div class="text-white">${message.msg}</div>
                 <span class="text-success d-flex justify-content-start" style="font-size: 10px;">${message.name}</span>
             </div>
         </div>
-        `)
-    }
-    if(message.tipe == "2"){
-        $("#Chatareaid").html($("#Chatareaid").html() +
-        `
+        `);
+            break;
+        case "2":
+            $("#Chatareaid").html($("#Chatareaid").html() +
+                `
         <div class="alert alert-success" role="alert">
             ${message.name} is Connected.
         </div>
-        `)
-    } 
-    if(message.tipe == "3"){
-        $("#Chatareaid").html($("#Chatareaid").html() +
-        `
+        `);
+            break;
+        case "3":
+            $("#Chatareaid").html($("#Chatareaid").html() +
+                `
         <div class="alert alert-danger" role="alert">
-            ${message.name} is disconnected.
+            ${message.name}.
         </div>
-        `)
+        `);
+            break;
     }
-    verificaronlines()
+    verifyOnlineUsers();
+    scrollElement();
+};
 
-}
-
-function scrollelement(){
+// Use camelCase for function names
+const scrollElement = () => {
     var element = document.getElementById("Chatareaid");
     element.scrollTop = element.scrollHeight;
-}
+};
